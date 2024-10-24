e6a3d882d02c9024e2dd878792d18477569742fe
Sohail Shoukat <sohailshoukat131@gmail.com>
2024-10-24 21:30:48 +0500

    Learn Js

---
+++ 01_Basic/index.js
@@ -0,0 +1,3 @@
+console.log("Hello World!");
+document.write("Hello World !");
+alert("Hello This is First Program of JavaScript Would You Like ? 💖");
---
+++ 02_Variables/index.js
@@ -0,0 +1,20 @@
+const accountId = 144144144;
+let accountEmail = "sohail@gmail.com";
+var accountPassword = "00112233";
+accountCity = "Jacobabad";
+let accountState;
+
+// accountId = 2; // This is constant variable
+// console.log(accountId);
+
+accountEmail = "google@gmail.com";
+accountPassword = "44556677";
+accountCity = "Karachi";
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
