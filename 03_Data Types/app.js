33992bf873e4411c0d635132232147ff309721b1
Sohail Shoukat <sohailshoukat131@gmail.com>
2024-10-25 21:24:02 +0500

    Learn Js

---
+++ 03_Data Types/app.js
@@ -0,0 +1,37 @@
+const accountId = 12345;
+let accountEmail = "sohail@gmail.com";
+var accountPassword = "12345";
+let accountState;
+// accountId = 2;
+
+
+// console.log(accountEmail);
+console.table([accountId, accountEmail,accountPassword, accountState]);
+
+
+
+"use strict" // Treat all  Js code as new version
+
+alert(3 + 3);
+console.log(3 + 3);
+ console.log("Sohail") ;
+
+ // Data Types;
+ let name = "sohail"; // String  => "" or ''
+ let age = 21; // Number   // 2 to power 53
+ let isLoggedIn = true; // Boolean
+ // bigint
+// null => Standalone Value
+// Undefined
+let checkNull = null;
+// console.log(checkNull)
+
+console.log(typeof "JavaScript");
+console.log(typeof 2010);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
+
+
