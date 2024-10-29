b3708e73e3aff054c26faba45907cb302340ee35
Sohail Shoukat <sohailshoukat131@gmail.com>
2024-10-29 17:58:27 +0500

    Learn Js

---
+++ 05_Operations/app.js
@@ -0,0 +1,37 @@
+// Operations
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 2);
+console.log(2 / 2);
+console.log(2 % 3);
+
+let firstName = "Sohail";
+let lastName = " Shoukat";
+let result = firstName + lastName;
+console.log(result);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+
+// But
+console.log(1 + 2 + "2");
+// output will 32
+
+console.log(true);
+console.log(+"");
+console.log(+true);
+// console.log(true+)  error
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
